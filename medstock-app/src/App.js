4d16bd098dffb386e5a3b1adcd446fdc702f8834
dnm10 <deeptinm10@gmail.com>
2025-01-12 11:02:23 +0530

    Routed Medicines Page

--- medstock-app/src/App.js
+++ medstock-app/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Header from './Components/Header';
 import Notifications from "./Components/Notifications";
 import FormPopup from './Components/FormPopup';
-import SideBar from './Components/Sidebar';
+import SideBar from './Components/SideBar';
 import Home from './Components/Home';
 import Inventory from './Components/Inventory';
 import Billing from './Components/Billing';
+import Medicines from './Components/Medicines';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -24,6 +25,7 @@ function App() {
         <Route path="/Notifications" element={<Notifications />} />
         <Route exact path="/Inventory" element={<Inventory/>}/>
         <Route path="/Billing" element={<Billing/>}/>
+        <Route path="/Medicines" element={<Medicines/>}/>
       </Routes>
   
       <FormPopup />
