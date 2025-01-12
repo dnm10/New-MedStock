48cadd7e4cb5986cd2f0cd8ceab0d4e38e1cc05e
dnm10 <deeptinm10@gmail.com>
2025-01-12 11:01:07 +0530

    Added Medicines file

---
+++ medstock-app/src/Components/Medicines.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Medicines.module.css';
+import '../App.css';
+
+export default function Medicines() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
