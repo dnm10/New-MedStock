21806e8e3bbe167763e614c79af6c147a0aa288d
dnm10 <deeptinm10@gmail.com>
2025-01-19 13:55:01 +0530

    File name

--- medstock-app/src/Components/Sidebar.js
+++ medstock-app/src/Components/Sidebar.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import '../App.css';
-import './Sidebar.css';
+import './SideBar.css';
 import mslogo from '../Assets/mslogo.png';
 import { Link } from 'react-router-dom';
 
